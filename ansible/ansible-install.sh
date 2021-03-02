08947b65fd3022278eed54db4f353b830d80f329
root <root@localhost.localdomain>
2021-03-02 12:57:13 -0500

    Cambios en los playbooks de ansible

--- ansible/ansible-install.sh
+++ ansible/ansible-install.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 
-#terraform init && #terraform apply
-
+#Lanzamos la ejecución de todos los libros
 ansible-playbook setup_master_node.yaml && ansible-playbook setup_worker_nodes.yaml && ansible-playbook setup_nfs-server.yaml && ansible-playbook setup_k8sdashboard.yaml
